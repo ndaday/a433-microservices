77e24091a2480d7e3e8ccbc098030628795f586e
ndaday <nur.andromeda@gmail.com>
2023-12-10 23:47:50 +0700

    add script and CI config

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# membuat docker image dari dockerfile dengan nama karsajobs_ui dan tag latest
+docker build -t ndaday/karsajobs_ui:latest .
+
+# melihat daftar image di lokal
+docker images
+
+# mengubah nama igame
+docker tag ndaday/karsajobs_ui:latest ghcr.io/ndaday/karsajobs_ui:latest
+
+# login ke github package
+echo $PAT | docker login ghcr.io --username phanatic --password-stdin
+
+# mengunggah image ke github package
+docker push ghcr.io/ndaday/karsajobs_ui:latest
